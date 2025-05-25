3abc9ddb2aa7a6cf292dd4b7fcb45925bd2604f0
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-05-25 23:43:00 +0530

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,43 @@
+ 
+ "use strict";  //  treat all js code as newer version
+
+ let name = "Anjali";
+ let age = 19;
+ let isAdmitted = false;
+ let salary = null;
+ let designation;
+
+ console.log(typeof name);
+ console.log(typeof age);
+ console.log(typeof isAdmitted);
+ console.log(typeof salary);
+ console.log(typeof designation);
+
+ console.log(typeof(name));
+ console.log(typeof(age));
+ console.log(typeof(isAdmitted));
+ console.log(typeof(salary));
+ console.log(typeof(designation));
+
+ // Primitive datatypes
+
+ // number=> range of datatype is 2 to the power 52 or 53 
+ // bigint=> Big value
+ // string=> ""
+ // boolean=> true => 1, /false => 0
+ // null=> standlone value represents empty value
+ // undefined
+ // symbol=> uniqueness
+
+
+ // mostly used datatype
+ // object
+
+ console.log(typeof null);  // object
+ console.log(typeof undefined);  // undefined
+ 
+ 
+
+ 
+
+ 

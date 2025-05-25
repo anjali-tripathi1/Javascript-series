24d54b559cc308606f1d438056acafbf360a93e9
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-05-25 21:07:00 +0530

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,28 @@
  
-//   Qu.1 What are the variables ? And How do we use variables in Javascript ?
-//     Ans. In JavaScript, variables are used to store data values or They are used for creating 
-//          space in memory for store data value. and for using variables in Javascript we declare variables. 
+//   Qu.1 What are the variables ? And How do we use variables in Javascript ?     
+//     Ans. In JavaScript, variables are used to store data values or They are used for creating  space in 
+//          memory for store data value. and for using variables in Javascript we declare variables.
 
+
+// We have some Variables
+// suppose we have created e-commerce Website and we have to give users uniqueId Email and some more options.
+
+  const accountId = 23451
+  var accountEmail = "Anjali@.com"
+  let accountPassword = "12345"
+  accountCity = "Jaipur"
+
+//   const accountId = 553322  (not allowed)
+
+accountEmail = "abc@.com"
+accountPassword = "212121"
+accountCity = "Bengluru"
+
+  console.log(accountId);
+  console.table([accountId, accountEmail, accountPassword, accountCity])
+
+  //  we don't use var because it has some problem it doesn't know about block scope and fuctional scope (Prefer not to use var )
+
+ 
+  
  

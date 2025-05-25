4ac800c3e5684a05cf711d16b1cd3a3308ac6a4a
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-05-25 20:08:47 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+ 
+//   Qu.1 What are the variables ? And How do we use variables in Javascript ?
+//     Ans. In JavaScript, variables are used to store data values or They are used for creating 
+//          space in memory for store data value. and for using variables in Javascript we declare variables. 
+
+ 

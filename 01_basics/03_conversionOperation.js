c0281f47ac424237d29ad08c9928d2c8274c5e7f
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-05-26 01:20:25 +0530

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,179 @@
+ // Conversion of Datatypes
+
+ let score = 33;
+ let score1 = "33";
+ let score2 = "33abc";
+ let score3 = null;
+ let score4 = undefined;
+ let score5 = -1/0;
+ let score6 = true;
+ let score7 = "Anjali";
+
+
+ // datatype print
+
+ console.log(typeof score);
+ console.log(typeof score1);
+ console.log(typeof score2);
+ console.log(typeof score3);
+ console.log(typeof score4);
+ console.log(typeof score5);
+ console.log(typeof score6);
+ console.log(typeof score7);
+
+// Number conversion
+
+ let valueInNumber = Number(score);  
+ let valueInNumber1 = Number(score1);  
+ let valueInNumber2 = Number(score2);  
+ let valueInNumber3 = Number(score3);  
+ let valueInNumber4 = Number(score4);  
+ let valueInNumber5 = Number(score5);  
+ let valueInNumber6 = Number(score6);  
+ let valueInNumber7 = Number(score7);  
+
+ console.log(valueInNumber);
+ console.log(valueInNumber1);
+ console.log(valueInNumber2);
+ console.log(valueInNumber3);
+ console.log(valueInNumber4);
+ console.log(valueInNumber5);
+ console.log(valueInNumber6);
+ console.log(valueInNumber7);
+ 
+
+ // String conversion
+
+ let valueInString1 = String(score1);
+ let valueInString2 = String(score2);
+ let valueInString3 = String(score3);
+ let valueInString4 = String(score4);
+ let valueInString5 = String(score5);
+ let valueInString6 = String(score6);
+ let valueInString7 = String(score7);
+
+ console.log(valueInString1);
+ console.log(valueInString2);
+ console.log(valueInString3);
+ console.log(valueInString4);
+ console.log(valueInString5);
+ console.log(valueInString6);
+ console.log(valueInString7);
+
+
+//  Boolean conversion
+
+ let score8 = false;
+ let score9 = 0;
+ let score10 = "";
+ let score11 = "Anjali";
+ let score12 = true;
+ let score13 = false 
+
+ console.log(typeof score8);
+ console.log(typeof score9);
+ console.log(typeof score10);
+ console.log(typeof score11);
+ console.log(typeof score12);
+ console.log(typeof score13);
+
+ let valueInBoolean = Boolean(score)
+ let valueInBoolean1 = Boolean(score1)
+ let valueInBoolean2 = Boolean(score2)
+ let valueInBoolean3 = Boolean(score3)
+ let valueInBoolean4 = Boolean(score4)
+ let valueInBoolean5 = Boolean(score5)
+ let valueInBoolean6 = Boolean(score6)
+ let valueInBoolean7 = Boolean(score7)
+ let valueInBoolean8 = Boolean(score8)
+ let valueInBoolean9 = Boolean(score9)
+ let valueInBoolean10 = Boolean(score10)
+ let valueInBoolean11 = Boolean(score11)
+ let valueInBoolean12 = Boolean(score12)
+ let valueInBoolean13 = Boolean(score13)
+
+ console.log(valueInBoolean);
+ console.log(valueInBoolean1);
+ console.log(valueInBoolean2);
+ console.log(valueInBoolean3);
+ console.log(valueInBoolean4);
+ console.log(valueInBoolean5);
+ console.log(valueInBoolean6);
+ console.log(valueInBoolean7);
+ console.log(valueInBoolean8);
+ console.log(valueInBoolean9);
+ console.log(valueInBoolean10);
+ console.log(valueInBoolean11);
+ console.log(valueInBoolean12);
+ console.log(valueInBoolean13);
+ 
+
+// ################### Operations ##################
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(5 + 2);
+console.log( 5 + (-2));
+console.log( 5 - 2);
+console.log( 5*3);
+console.log(5**2);
+console.log(5**3);
+console.log(5/2);
+console.log(5%2);
+console.log(5/0);
+console.log(0/3);
+
+console.log(5+(+true));
+console.log(5+(-true));
+console.log(5-(+true));
+console.log(5-(-true));
+console.log(5*(true));
+console.log(5*(-true));
+console.log(5/(2*2+true));
+console.log(9/(2*2-true));
+console.log(-25/(2*2+true));
+console.log(5*false);
+console.log(5*-false);
+console.log(5+false);
+console.log(5-false);
+console.log(5/+false);
+console.log(5/-false);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" - 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
+ 

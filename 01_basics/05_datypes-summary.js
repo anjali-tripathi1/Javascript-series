268ed26c4b498dee484367986010428557b3c30d
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-05 11:07:20 +0530

    summary

---
+++ 01_basics/05_datypes-summary.js
@@ -0,0 +1,12 @@
+  // All diffrence between parts are bases on that whatever data are and how they are stored in memory and how can we access our data.
+
+  // #Primitive
+
+//   as many as primitive datypes are call by value, means when you copy them from somewhere then you are not provided original data (memory's refrence), these are copied and given and all changes that you do it is in the copy.
+
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt 
+
+
+
+// #Refrence (Non Primitive)
+// All values in memory that can be refrenced directly 

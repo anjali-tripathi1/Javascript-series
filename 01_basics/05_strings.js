ecfc468269e2e0ba5acf9f569307edcaac207da0
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-07 23:36:24 +0530

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,2 +1,25 @@
  
+ const name = "Anjali"
+ const repoCount = 10
+
+ console.log(name +  repoCount +  " value");   // this is outdated
+
+
+ console.log(`My name is :- ${name} and my repoCount is :- ${repoCount}`);
+
+ // backticks (``) use for string interpolation.
+
+
+ // one more way to declare string
+
+ const gameName = new String('Anjali')
+ // when we write string in console they have position and these are keyvalue pairs 0 is key and A is value. string is object. 
+
+
+ // if we want any property we know that we have keyvalue pairs so we will get property easily.
+ console.log(gameName[0]);    // we want to access key.
+
+ // we can also access prototype. 
+ console.log(gameName.__proto__);
+ 
  

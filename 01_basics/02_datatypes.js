16ad07974339f5a2db94b76a5bc8e5fcc4572690
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-11 21:53:52 +0530

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -13,11 +13,29 @@
  console.log(typeof salary);
  console.log(typeof designation);
 
+
+
  console.log(typeof(name));
  console.log(typeof(age));
  console.log(typeof(isAdmitted));
  console.log(typeof(salary));
  console.log(typeof(designation));
+
+ 
+//    one more way to declare datatypes
+
+const sname = new String("Anjali");
+ console.log(`value of string :- ${sname} and Datatype is :- ${typeof sname}`);
+
+const nname = new Number(15);
+console.log(`value of number :- ${nname} and Datatype is :- ${typeof nname}`);
+
+const bisLoggedIn = new Boolean(true);
+console.log(`value of boolean :- ${bisLoggedIn} and Datatype is :- ${typeof bisLoggedIn}`);
+
+const outsideTemp = null;
+console.log(`value of null :- ${outsideTemp} and Datatype is :- ${typeof outsideTemp}`);
+
 
  // Primitive datatypes
 

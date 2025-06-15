37a98d7a64ff14fa81894e72f3fe2e790dc0c6b5
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-15 09:51:51 +0530

    1

--- practice.js
+++ practice.js
@@ -15,19 +15,440 @@
  const accountCity = "jaipur"
  console.log(accountCity);
 
- let value = 5
+ 
+ 
+ 
+
+// datatypes
+
+ const number = 100
+ console.log(`value of number :- ${number} and datatype is :- ${typeof number}`);
+ const newnumber = new Number(200)
+console.log(`value of newnumber :- ${newnumber} and datatype is :- ${typeof newnumber}`);
+
+
+const string = "Anjali"
+console.log(`value of string :- ${string} and datatype is :- ${typeof string}`);
+const newstring = new String("Saroj")
+console.log(`value of newstring :- ${newstring} and datatype is :- ${typeof newstring}`);
+
+ 
+const boolean = true
+console.log(`value of boolean :- ${boolean} and datatype is :- ${typeof boolean}`);
+const newboolean = new Boolean(false)
+console.log(`value of newboolean :- ${newboolean} and datatype is :- ${typeof newboolean}`);
+
+
+const salary = null
+console.log(`value of salary :- ${salary} and datatype is :- ${typeof salary}`);
+const newsalary =  null
+console.log(`value of newsalary :- ${newsalary} and datatype is :- ${typeof newsalary}`);
+
+
+let designation;
+console.log(designation);
+const newdesignation = undefined
+console.log(`value of newdesignation :-  ${newdesignation} and datatype is :- ${typeof newdesignation}`);
+
+
+// conversion 
+
+let score = 33
+let score1 = "33"
+let score2 = "33abc"
+let score3 =  true
+let score4 =  null
+let score5 =  undefined
+let score6 =  -1/0
+let score7 =  "Anjali"
+let score8 =  ""
+let score9 = false
+let score10 = 0
+let score11 = 1
+
+
+console.log(`Datatype of score :- ${typeof score} and value is :- ${score}`);
+console.log(`Datatype of score1 :- ${typeof score1} and value is :- ${score1}`);
+console.log(`Datatype of score2 :- ${typeof score2} and value is :- ${score2}`);
+console.log(`Datatype of score3 :- ${typeof score3} and value is :- ${score3}`);
+console.log(`Datatype of score4 :- ${typeof score4} and value is :- ${score4}`);
+console.log(`Datatype of score5 :- ${typeof score5} and value is :- ${score5}`);
+console.log(`Datatype of score6 :- ${typeof score6} and value is :- ${score6}`);
+console.log(`Datatype of score7 :- ${typeof score7} and value is :- ${score7}`);
+console.log(`Datatype of score8 :- ${typeof score8} and value is :- ${score8}`);
+console.log(`Datatype of score9 :- ${typeof score9} and value is :- ${score9}`);
+console.log(`Datatype of score10 :- ${typeof score10} and value is :- ${score10}`);
+console.log(`Datatype of score11 :- ${typeof score11} and value is :- ${score11}`);
+
+
+
+let valueInNumber = Number(score)
+let valueInNumber1 = Number(score1)
+let valueInNumber2 = Number(score2)
+let valueInNumber3 = Number(score3)
+let valueInNumber4 = Number(score4)
+let valueInNumber5 = Number(score5)
+let valueInNumber6 = Number(score6)
+let valueInNumber7 = Number(score7)
+let valueInNumber8 = Number(score8)
+let valueInNumber9 = Number(score9)
+let valueInNumber10 = Number(score10)
+let valueInNumber11 = Number(score11)
+
+
+
+console.log(`Datatype of valueInNumber :- ${typeof valueInNumber} and value is ${valueInNumber}`);
+console.log(`Datatype of valueInNumber1 :- ${typeof valueInNumber1} and value is ${valueInNumber1}`);
+console.log(`Datatype of valueInNumber2 :- ${typeof valueInNumber2} and value is ${valueInNumber2}`);
+console.log(`Datatype of valueInNumber3 :- ${typeof valueInNumber3} and value is ${valueInNumber3}`);
+console.log(`Datatype of valueInNumber4 :- ${typeof valueInNumber4} and value is ${valueInNumber4}`);
+console.log(`Datatype of valueInNumber5 :- ${typeof valueInNumber5} and value is ${valueInNumber5}`);
+console.log(`Datatype of valueInNumber6 :- ${typeof valueInNumber6} and value is ${valueInNumber6}`);
+console.log(`Datatype of valueInNumber7 :- ${typeof valueInNumber7} and value is ${valueInNumber7}`);
+console.log(`Datatype of valueInNumber8 :- ${typeof valueInNumber8} and value is ${valueInNumber8}`);
+console.log(`Datatype of valueInNumber9 :- ${typeof valueInNumber9} and value is ${valueInNumber9}`);
+console.log(`Datatype of valueInNumber10 :- ${typeof valueInNumber10} and value is ${valueInNumber10}`);
+console.log(`Datatype of valueInNumber11 :- ${typeof valueInNumber11} and value is ${valueInNumber11}`);
+
+ 
+ 
+ let valueInString = String(score)
+ let valueInString1 = String(score1)
+ let valueInString2 = String(score2)
+ let valueInString3 = String(score3)
+ let valueInString4 = String(score4)
+ let valueInString5 = String(score5)
+ let valueInString6 = String(score6)
+ let valueInString7 = String(score7)
+ let valueInString8 = String(score8)
+ let valueInString9 = String(score9)
+ let valueInString10 = String(score10)
+ let valueInString11 = String(score11)
+
+ console.log(`Datatype of valueInString :- ${typeof valueInString} and value is :- ${valueInString}`);
+ console.log(`Datatype of valueInString1 :- ${typeof valueInString1} and value is :- ${valueInString1}`);
+ console.log(`Datatype of valueInString2 :- ${typeof valueInString2} and value is :- ${valueInString2}`);
+ console.log(`Datatype of valueInString3 :- ${typeof valueInString3} and value is :- ${valueInString3}`);
+ console.log(`Datatype of valueInString4 :- ${typeof valueInString4} and value is :- ${valueInString4}`);
+ console.log(`Datatype of valueInString5 :- ${typeof valueInString5} and value is :- ${valueInString5}`);
+ console.log(`Datatype of valueInString6 :- ${typeof valueInString6} and value is :- ${valueInString6}`);
+ console.log(`Datatype of valueInString7 :- ${typeof valueInString7} and value is :- ${valueInString7}`);
+ console.log(`Datatype of valueInString8 :- ${typeof valueInString8} and value is :- ${valueInString8}`);
+ console.log(`Datatype of valueInString9 :- ${typeof valueInString9} and value is :- ${valueInString9}`);
+ console.log(`Datatype of valueInString10 :- ${typeof valueInString10} and value is :- ${valueInString10}`);
+ console.log(`Datatype of valueInString11 :- ${typeof valueInString11} and value is :- ${valueInString11}`);
+ 
+
+
+ let valueInBoolean = Boolean(score)
+ let valueInBoolean1 = Boolean(score1)
+ let valueInBoolean2 = Boolean(score2)
+ let valueInBoolean3 = Boolean(score3)
+ let valueInBoolean4 = Boolean(score4)
+ let valueInBoolean5 = Boolean(score5)
+ let valueInBoolean6 = Boolean(score6)
+ let valueInBoolean7 = Boolean(score7)
+ let valueInBoolean8 = Boolean(score8)
+ let valueInBoolean9 = Boolean(score9)
+ let valueInBoolean10 = Boolean(score10)
+ let valueInBoolean11 = Boolean(score11)
+
+
+
+ console.log(`Datatype of valueInBoolean :- ${typeof valueInBoolean} and value is :- ${valueInBoolean}`);
+ console.log(`Datatype of valueInBoolean1 :- ${typeof valueInBoolean1} and value is :- ${valueInBoolean1}`);
+ console.log(`Datatype of valueInBoolean2 :- ${typeof valueInBoolean2} and value is :- ${valueInBoolean2}`);
+ console.log(`Datatype of valueInBoolean3 :- ${typeof valueInBoolean3} and value is :- ${valueInBoolean3}`);
+ console.log(`Datatype of valueInBoolean4 :- ${typeof valueInBoolean4} and value is :- ${valueInBoolean4}`);
+ console.log(`Datatype of valueInBoolean5 :- ${typeof valueInBoolean5} and value is :- ${valueInBoolean5}`);
+ console.log(`Datatype of valueInBoolean6 :- ${typeof valueInBoolean6} and value is :- ${valueInBoolean6}`);
+ console.log(`Datatype of valueInBoolean7 :- ${typeof valueInBoolean7} and value is :- ${valueInBoolean7}`);
+ console.log(`Datatype of valueInBoolean8 :- ${typeof valueInBoolean8} and value is :- ${valueInBoolean8}`);
+ console.log(`Datatype of valueInBoolean9 :- ${typeof valueInBoolean9} and value is :- ${valueInBoolean9}`);
+ console.log(`Datatype of valueInBoolean10 :- ${typeof valueInBoolean10} and value is :- ${valueInBoolean10}`);
+ console.log(`Datatype of valueInBoolean11 :- ${typeof valueInBoolean11} and value is :- ${valueInBoolean11}`);
+ 
+ 
+// Operations 
+
+let value = 5
  let negValue = -value
 console.log(negValue);
 
+console.log(5 + 3);
+console.log(5 - 3);
+console.log(5 + (-3));
+console.log(3 + (-3));
+console.log(3 + (-5));
+console.log(5 * (-3));
+console.log(5 * 3);
+console.log(5 ** 3);
+console.log(5 ** 2);
+console.log(5 / 3);
+console.log(5 % 3);
+console.log(0 / 5);
+console.log(5 / 0);
+
+
+console.log(`${5+true} and ${5+(-true)}`);
+console.log(`${5-true} and ${5-(-true)}`);
+console.log(`${5+false} and ${5+(-false)}`);
+console.log(`${5-false} and ${5-(-false)}`);
+console.log(`${5*false} and ${5*(-false)}`);
+console.log(`${5*true} and ${5*(-true)}`);
+console.log(`${5/true} and ${5/(-true)}`);
+console.log(`${5/false} and ${5/(-false)}`);
+console.log(`${"1" + 2 } and ${"1" + (-2) }`);
+console.log(`${"1" - 2 } and ${"1" - (-2) }`);
+console.log(`${"1" + 2 + 3 } and ${"1" + (-2) + 3 }`);
+console.log(`${"1" - 2 + 3 } and ${"1" - (-2) + 3 }`);
+console.log(`${1 + 2 + "3"} and ${"1" + (-2) + "3" }`);
+console.log(`${1 - 2 + "3"} and ${"1" - (-2) + "3" }`);
+console.log(`${+true}`);
+console.log(`${-true}`);
+console.log(`${+false}`);
+console.log(`${-false}`);
+console.log(`${1 + "" } and ${typeof (1 + "")}`);
+console.log(`${1 - "" } and ${typeof (1 - "")}`);
+
+
+let gamecounter1 = 100
+let gamecounter2 = 200
+console.log(`value of counter1 :- ${gamecounter1} and ${gamecounter2}`);
+++gamecounter1
+gamecounter2++
+console.log(`value of counter2 :- ${gamecounter1} and ${gamecounter2}`);
+
+
+// comparison 
+
+//  console.log("2" > "1");
+//  console.log("2" < "1");
+//  console.log("2" >= "1");
+//  console.log("2" <= "1");
+//  console.log("2" == "1");
+//  console.log("2" != "1");
+ 
+//  console.log(2 > 1);
+//  console.log(2 < 1);
+//  console.log(2 >= 1);
+//  console.log(2 <= 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
+
+//  console.log(true > false);
+//  console.log(true < false);
+//  console.log(true >= false);
+//  console.log(true <= false);
+//  console.log(true == false);
+//  console.log(true != false);
+
+
+//  console.log(null > 0);
+//  console.log(null < 0);
+//  console.log(null >= 0);
+//  console.log(null <= 0);
+//  console.log(null == 0);
+//  console.log(null != 0);
+
+
+//  console.log(undefined > 0);
+//  console.log(undefined < 0);
+//  console.log(undefined >= 0);
+//  console.log(undefined <= 0);
+//  console.log(undefined == 0);
+//  console.log(undefined != 0);
  
 
+//  console.log("2" == 2 );
  
+//  console.log("2" === 2);
+
+ console.log("2" > 1);
+ console.log("2" < 1);
+ console.log("2" <= 1);
+ console.log("2" >= 1);
+ console.log("2" == 1);
+ console.log("2" != 1);
+
+ console.log(2 > true);
+ console.log(2 < true);
+ console.log(2 >= true);
+ console.log(2 <= true);
+ console.log(2 == true);
+ console.log(2 != true);
+
+ console.log("2" > true);
+ console.log("2" < true);
+ console.log("2" >= true);
+ console.log("2" <= true);
+ console.log("2" == true);
+ console.log("2" != true);
  
+ const Id = Symbol('123')
+ const anotherId = Symbol('123')
  
+ console.log(Id);
+ console.log(anotherId);
+
+ console.log(Id == anotherId);
+ console.log(Id === anotherId);
  
+ const bigNumber = 22434567743322221111n
+ console.log(bigNumber);
+ console.log(typeof bigNumber);
+
+ const arrayNumber = [0, 1, 2, 3, 4, 5]
+ const arrayString = ["Anjali", "Saroj", "Aditya"]
+ const arrayBoolean = [true, false]
+ const arraynull = [null] 
+ const arrayCity = ["Jaipur"]
+ const arrayAccount = [undefined]
  
+ console.table([arrayNumber, arrayString, arrayBoolean, arraynull, arrayCity, arrayAccount])
+ console.table(typeof arrayAccount);
+ console.table(typeof arrayNumber);
+ console.table(typeof arraynull);
+ console.table(typeof arrayBoolean);
+ console.table(typeof arrayString);
+ console.table(typeof arrayCity);
 
  
+ let myobj ={
+    name: "Anjali",
+    age: 19,
+    isLoggedIn: true,
+    outsideTemp: null,
+    accountCity: undefined,
+    accountnumber: [2, 1, 4, 5, 3],
+    function(){
+        console.log("Hello Anjali");
+        
+    },
+    myfiles: {
+        fullName: "Anjali Tripathi"
+    }
+ }
+ 
+ console.log(typeof myobj);
+ console.log(myobj);
+
+ const myFunction = function(){
+     console.log("hello world");
+     
+ }
+
+ console.log(myFunction());
+ 
+ 
+ // memory 
+
+ let myYoutubeName = "Anjali@google.com"
+ 
+ let anothername = myYoutubeName
+ anothername = "honeyboney"
+console.log(myYoutubeName);
+console.log(anothername);
+
+
+let myNumber = 12345
+
+let anothermyNumber = myNumber
+anothermyNumber = 12345.35
+console.log(myNumber);
+console.log(anothermyNumber);
+
+let myBooleanvalue = true
+
+let anothermyBooleanvalue = myBooleanvalue
+anothermyBooleanvalue = false
+console.log(myBooleanvalue);
+console.log(anothermyBooleanvalue);
+
+
+let userOne = {
+    name: "Anjali",
+    age: 19,
+    isLoggedIn: true,
+    email: "Anjali@google.com"
+}
+
+let userTwo = userOne
+userTwo.email = "Saroj7398.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+userTwo.age = 25
+console.log(userOne.age);
+console.log(userTwo.age);
+
+const myString = "Anjali"
+console.log(myString);
+
+const myNewString = new String("Saroj Tripathi")
+console.log(myNewString);
+// 0 : "S"
+// 1 : "a"
+// 2 : "r"
+// 3 : "o"
+// 4 : "j"
+// length : 5
+
+console.log(myNewString[2]);
+console.log(myNewString.length);
+console.log(myNewString.__proto__);
+
+console.log(myNewString.toUpperCase());
+console.log(myNewString.toLowerCase());
+console.log(myNewString.charAt(0));
+console.log(myNewString.indexOf('r'));
+console.log(myNewString.substring(0, 7));
+
+const mynewvalue = myNewString.substring(0, 8)
+console.log(mynewvalue);
+
+const anothermynewvalue = myNewString.slice(-3, 4)
+console.log(anothermynewvalue);
+
+const anothermynewvalueOne = "    Anjali     "
+console.log(anothermynewvalueOne.trim());
+console.log(anothermynewvalueOne.trimStart());
+console.log(anothermynewvalueOne.trimEnd());
+
+
+const url = "https://Anjali.com/anjali%20tripathi"
+
+console.log(url.replace('%20',  '-'));
+console.log(url.replace('%20',  ' '));
+console.log(url.replace('%20',  '@#'));
+
+console.log(url.includes('Anjali'));
+console.log(url.includes('Saroj'));
+
+
+console.log(myNewString.split(' '));
+
+const cartoonName = new String("Saroj-Tr-com")
+console.log(cartoonName.split('-'));
+console.log(cartoonName.split(' '));
+console.log(cartoonName.split('##'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
  
  

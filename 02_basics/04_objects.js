ab2b8f16d07b9c95bd60e43c8c411cad2dbf55b5
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-17 23:11:28 +0530

    objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -55,6 +55,7 @@ const regularUser = {
  
 
  // one more syntax that we use for combine
+ // spread method
  const obj7 = {...obj1, ...obj2, ...obj3}
  console.log(obj7);
  
@@ -62,4 +63,74 @@ const regularUser = {
 // one more syntax which we will use when values come from database.
 // suppose users have come when users come from  most of the database then users come array of objects.
   
+ const users = [
+      {
+        id : 1,
+        email : "h@email.com"
+      },
+
+       {
+        id : 2,
+        email : "a@email.com"
+      },
+
+       {
+        id : 3,
+        email : "b@email.com"
+      }
+ ]
+     // so we have many objects inside array or you have to do loop through or print any value
+     console.log(users[1].email);
+     console.log(users[0].id);
+     console.log(users[2].id);
+
+
+     // sometimes we need this type of method.
+     console.log(tinderUser);
+     console.log(Object.keys(tinderUser));
+     // we write Object and we want all the keys of Object whenever we use Object method so it doesn't know that on whow does it want to use the method so we have to write the value, and the value which we get that's datatype is array so we have taken all values and keep them in array now you can put a loop on it if we want and that can be use many places. just like keys we can also find the value.
+     console.log(Object.values(tinderUser));
+     // and another basic properties we can use like length, position, keys and values.
+     console.log(Object.entries(tinderUser));
+     // here we get array inside array, every keyvalue has created as an array so its first value will be key and second value will be value.
+     
+     // 
+    //  sometimes you do complete loopthrough inside objects and after doing loop value is coming out after extracting the value that value doesn't exist if the value is not exist so value can be crashed. so either you see that manually or you can also ask that do you have this value so it's result appears in boolean 
+    console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+    
+
+    // destructureing so destructureing is doing in array and bjects too so here we talk about destructuring in objects.
+
+    const course = {
+        Coursename : "Js Series",
+        Price : "999",
+        CourseInstructor : "Anjali"
+    }
+
+    // so we can access value like this
+        // console.log(course.CourseInstructor);
+
+    //  we have one more syntax which we can use 
+     const {CourseInstructor : Instructor} = course
+    //  console.log(CourseInstructor);
+     console.log(Instructor);
+     // here we don't need to write course again. if you don't want to write such big value (CourseInstructer) so you can give another name so now we call this Instructor in place of CourseInstructor so this is the syntax so we can do any value's destructure. so this is called that how we can do object destructure.
+    
+     const {Price : P} = course
+    //  console.log(Price);
+     console.log(P);
+     
+     
+
+
+     // In react we have some method 
+     
+     
+     
+     
+     
+      
+     
+     
+
  

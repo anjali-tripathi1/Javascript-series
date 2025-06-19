e22f008c6bd44d09c4745fa993fe429503f96126
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-19 19:07:45 +0530

    upload

--- 03_basics/01_fuctions.js
+++ 03_basics/01_fuctions.js
@@ -52,3 +52,38 @@
 
     // (username === undefined) = (!username) both syntax are same
     // if we want to avoid this situation
+
+    // More parameter that we use
+    function calculateCartPrice(val1, val2, ...num1){
+          return num1
+    }
+
+    console.log(calculateCartPrice(200, 300, 400, 500, 2000))
+
+    const user = {
+        username : "Anjali",
+        Price : 199
+    }
+
+    function handleobject(anyobject){
+        console.log(`Username is ${anyobject.username} and Price is ${anyobject.Price}`);
+        
+    }
+
+    handleobject(user)
+    // we can also use object like this
+    handleobject({
+         username : "Anjali",
+         Price : 399
+    })
+    
+ const myNewArray = [100, 200, 300, 400, 500]
+
+ function returnSecondValue(getArray){
+     return getArray[2]
+ }
+
+ console.log(returnSecondValue(myNewArray))
+ console.log(returnSecondValue([200, 300, 500, 1000, 1500]));
+
+ 

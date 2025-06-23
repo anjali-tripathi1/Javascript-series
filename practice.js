c76e30ecc6af57ba52bf8d7e5734e6de7da0eedd
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-23 23:18:20 +0530

    1

--- practice.js
+++ practice.js
@@ -1,36 +1,40 @@
 
-  
+function addTwoNumbers (number1, number2){
+     console.log(number1 + number2);
+     
+}
+
+addTwoNumbers(4, 5)
+
+function addTwoNumbers (number1, number2) {
+     let result = number1 + number2
+     return result
+    //  return number1 + number2
+}
+
+// console.log(addTwoNumbers(3, 4))
+
+const result = addTwoNumbers(3, 4)
+console.log("Result:", result);
 
 
+function loginUserMessage (username = "Sam"){
+    if(username === undefined){
+         console.log("Please enter your name");
+         
+    }
 
+    if(!username){
+         console.log("Please enter your name");
+         
+    }
+     console.log(`${username} just Logged In`);
+     
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+loginUserMessage("Anjali")
+loginUserMessage("")
+loginUserMessage()
 
 
 

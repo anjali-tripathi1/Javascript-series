3e99d363fe867c84cca6dc5b151729589c39796c
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-06-24 17:59:28 +0530

    Update practice.js

--- practice.js
+++ practice.js
@@ -376,6 +376,16 @@ const userEmail = "false"
      console.log(element)
  }
 
+ for (let i = 0; i <= 10; i++) {
+     const element = i;
+     if(element === 5){
+         console.log("5 is the best number")
+     }
+     console.log(element);
+     
+    
+}
+
 
 
 

fe4aa18aa9b226a975c15e4831a0973f3e74241f
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-01 21:26:37 +0530

    upload

--- practice.js
+++ practice.js
@@ -65,153 +65,305 @@
     
 // }
 
-const userLoggedIn = true
-const debitCard = true
-if (userLoggedIn && debitCard && 2==2) {
-    console.log(`user logged and debitcard is true`);
-    
-}
-
-const loggedInFromEmail = true
-const loggedInFromGoggle = false
-if (loggedInFromEmail || loggedInFromGoggle) {
-    console.log(`user logged in`);
-    
-}
-
-let val1;
-val1 = 5 ?? 10
-val1 = null ?? 10
-val1 = undefined ?? 4 ?? 5
-console.log(val1);
-
-const IceTeaPrice = 100
-IceTeaPrice >= 80 ? console.log("less than 50") : console.log("more than 80");
-IceTeaPrice <= 80 ? console.log("less than 50") : console.log("more than 80");
-
-const month = "feb"
-switch (month) {
-    case "jan":
-        console.log("January");
-     break;
-
-     case "feb":
-        console.log("Feb");
-     break;
-
-     case "march":
-        console.log("March");
-     break;
-
-     case "april":
-        console.log("April");
-     break;
-
-     case "may":
-        console.log("May");
-     break;
-
-    default:
-        console.log(`month is not print`);
-        
-        break;
-}
-
-// const userEmail = "A@Anjali.ai"
-// const userEmail = ""
-const userEmail = []
-
-if (userEmail) {
-    console.log(`got userEmail`);
-    
-} else {
-    console.log(`don't have userEmail`);
-    
-}
-
-const emptyArray = []
-if (emptyArray.length === 0) {
-    console.log(`array is empty`);
-    
-}
-
-const myObject = {}
-// if (Object.keys(myObject).length === 0) {
-//     console.log(`object is empty`);
+// const userLoggedIn = true
+// const debitCard = true
+// if (userLoggedIn && debitCard && 2==2) {
+//     console.log(`user logged and debitcard is true`);
     
 // }
-if (Object.keys(myObject).length === 0) {
-    console.log(`obj is empty`);
-    
-}
 
-for (let index = 0; index <= 10; index++) {
-    const element = index;
-    console.log(element);
+// const loggedInFromEmail = true
+// const loggedInFromGoggle = false
+// if (loggedInFromEmail || loggedInFromGoggle) {
+//     console.log(`user logged in`);
     
-}
+// }
 
-for (let i = 0; i <= 10; i++) {
-    const element = i;
-    if (element === 5) {
-         console.log(`5 is best number`);
+// let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 4 ?? 5
+// console.log(val1);
+
+// const IceTeaPrice = 100
+// IceTeaPrice >= 80 ? console.log("less than 50") : console.log("more than 80");
+// IceTeaPrice <= 80 ? console.log("less than 50") : console.log("more than 80");
+
+// const month = "feb"
+// switch (month) {
+//     case "jan":
+//         console.log("January");
+//      break;
+
+//      case "feb":
+//         console.log("Feb");
+//      break;
+
+//      case "march":
+//         console.log("March");
+//      break;
+
+//      case "april":
+//         console.log("April");
+//      break;
+
+//      case "may":
+//         console.log("May");
+//      break;
+
+//     default:
+//         console.log(`month is not print`);
+        
+//         break;
+// }
+
+// // const userEmail = "A@Anjali.ai"
+// // const userEmail = ""
+// const userEmail = []
+
+// if (userEmail) {
+//     console.log(`got userEmail`);
+    
+// } else {
+//     console.log(`don't have userEmail`);
+    
+// }
+
+// const emptyArray = []
+// if (emptyArray.length === 0) {
+//     console.log(`array is empty`);
+    
+// }
+
+// const myObject = {}
+// // if (Object.keys(myObject).length === 0) {
+// //     console.log(`object is empty`);
+    
+// // }
+// if (Object.keys(myObject).length === 0) {
+//     console.log(`obj is empty`);
+    
+// }
+
+// for (let index = 0; index <= 10; index++) {
+//     const element = index;
+//     console.log(element);
+    
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element === 5) {
+//          console.log(`5 is best number`);
          
-    }
-    console.log(element);
+//     }
+//     console.log(element);
     
-}
+// }
 
-for (let i = 1; i <= 10; i++) {
-      console.log(`outer loop value: ${i}`);
+// for (let i = 1; i <= 10; i++) {
+//       console.log(`outer loop value: ${i}`);
       
     
-    for (let j = 1; j <= 10; j++) {
-        console.log(`Inner loop value: ${i} and inner loop ${j}`)
-        console.log(i + '*' + j  + ' = ' + i*j);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`Inner loop value: ${i} and inner loop ${j}`)
+//         console.log(i + '*' + j  + ' = ' + i*j);
         
-    }
-}
+//     }
+// }
 
-const myArray = ["shaktiman", "flash", "poga"]
+// const myArray = ["shaktiman", "flash", "poga"]
 
-for (let i = 0; i < myArray.length; i++) {
-    const element = myArray[i];
-    console.log(element);
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element);
    
-}
+// }
 
-for (let i = 0; i <= 20; i++) {
-    if (i == 5) {
-         console.log(`Detected 5`);
-         continue
-    }
-    console.log(i);
+// for (let i = 0; i <= 20; i++) {
+//     if (i == 5) {
+//          console.log(`Detected 5`);
+//          continue
+//     }
+//     console.log(i);
     
     
-}
+// }
 
-let index = 0
-while (index <= 10) {
-    console.log(`value of index is: ${index}`);
-    // index = index + 2
-    // index++
-    index = index + 1
-}
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is: ${index}`);
+//     // index = index + 2
+//     // index++
+//     index = index + 1
+// }
 
-const myArr = ["flash", "shaktiman", "doga"]
-let arr = 0 
-while (arr < myArr.length) {
-    console.log(`value is ${myArr[arr]}`);
-    arr++   
-}
+// const myArr = ["flash", "shaktiman", "doga"]
+// let arr = 0 
+// while (arr < myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr++   
+// }
 
 // let score = 10
 // do {
-//     console.log(``);
+//     console.log(`value is ${score}`);
+//     score++
+// } while (score <= 10);
+
+// let score1 = 11
+// do {
+//     console.log(`score1 is ${score1}`);
+//     score1++
+// } while (score1 <= 10);
+
+// const myArray = ["shaktiman", "doga", "cat"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`valus is ${myArray[arr]}`);
+//     arr++
+// }
+
+// const myArr = [1, 2, 3, 4, 5]
+// for (const arr of myArr) {
+//     console.log(`value is ${arr}`);
     
-// } while (sco);
+// }
+
+// const greetings = "Hello world!"
+// for (const greet of greetings) {
+//      console.log(`value is ${greet}`);
+     
+// }
+
+// const myObj = {
+//     name : "Anjali",
+//     age : 20
+// }
+// for (const obj of myObj) {
+//     console.log(`my values are ${obj}`);
+    
+// }
 
 
+
+// console.log(map);
+// for (const key of map) {
+//     console.log(key);
+    
+// }
+
+// for (const [key, value] of map) {
+//     console.log(key, ":", value);
+    
+// }
+
+
+
+// const myObject = {
+//     js : "Javascript",
+//     java : "Java",
+//     py : "python",
+//     rb : "ruby",
+//     cpp : "C++"
+// }
+
+// for (const key in myObject) {
+//     // console.log(myObject[key]);
+//     console.log(`my key is ${key} and value is ${myObject[key]}`);
+        
+// }
+
+// const myArray = ["js", "java", "python", "ruby", "cpp"]
+// for (const key in myArray) {
+//     console.log(myArray[key]);
+    
+// }
+
+// const map = new Map()
+// map.set('In', "India")
+// map.set('Fr', "France")
+// map.set('pa', "Paris")
+// map.set('USA', "United States of America")
+
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
+
+// const coding = ["js", "java", "ruby", "cpp", "python"]
+// coding.forEach(function (val) {
+//     console.log(val);
+    
+// })
+
+// coding.forEach( (item) => {
+//     console.log(`values are :${item}`);
+    
+// })
+
+// function myvalue (item) {
+//     console.log(item);
+    
+// }
+
+// coding.forEach(myvalue)
+
+// coding.forEach( (item, index, arr) => {
+//      console.log(item, index, arr)
+// })
+
+// const mycoding = [
+//     {
+//         coursename : "Js Series",
+//         price : 399
+//     },
+
+//     {
+//         coursename : "Java",
+//         price : 599  
+//     },
+
+//     {
+//         coursename : "Mobile development",
+//         price : 5999  
+//     },
+
+//     {
+//         coursename : "Data science",
+//         price : 12999  
+//     },
+// ]
+
+// mycoding.forEach( (item) => {
+//     console.log(item.coursename);
+    
+// })
+
+// const myStrings = [
+//     "Anjali",
+//     "Saroj",
+//     "Aditya"
+// ]
+
+// myStrings.forEach( (item) => {
+//     console.log(item)
+// })
+
+const coding = ["javascript", "Java", "python", "cpp", "swift"]
+const values = coding.forEach( (item) => {
+    //  console.log(item)
+     return item
+})
+
+console.log(values);
+
+// const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const myArr = myNums.filter( (num) => num > 4)
+// console.log(myArr);
+    
+    
 
 
 

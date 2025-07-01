1b28b9b281c0a59c6c3a3b33a4ae05a121ad2f2f
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-01 09:47:11 +0530

    upload

--- practice.js
+++ practice.js
@@ -117,13 +117,76 @@ switch (month) {
         break;
 }
 
-const userEmail = "A@Anjali.ai"
+// const userEmail = "A@Anjali.ai"
+// const userEmail = ""
+const userEmail = []
 
 if (userEmail) {
     console.log(`got userEmail`);
     
 } else {
     console.log(`don't have userEmail`);
+    
+}
+
+const emptyArray = []
+if (emptyArray.length === 0) {
+    console.log(`array is empty`);
+    
+}
+
+const myObject = {}
+// if (Object.keys(myObject).length === 0) {
+//     console.log(`object is empty`);
+    
+// }
+if (Object.keys(myObject).length === 0) {
+    console.log(`obj is empty`);
+    
+}
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element === 5) {
+         console.log(`5 is best number`);
+         
+    }
+    console.log(element);
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+      console.log(`outer loop value: ${i}`);
+      
+    
+    for (let j = 1; j <= 10; j++) {
+        console.log(`Inner loop value: ${i} and inner loop ${j}`)
+        console.log(i + '*' + j  + ' = ' + i*j);
+        
+    }
+}
+
+const myArray = ["shaktiman", "flash", "poga"]
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+   
+}
+
+for (let i = 0; i <= 20; i++) {
+    if (i == 5) {
+         console.log(`Detected 5`);
+         continue
+    }
+    console.log(i);
+    
     
 }
 

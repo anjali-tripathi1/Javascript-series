7adcb30eb099f013d7b7402291e14432fa290d62
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-05 19:40:22 +0530

    upload

--- practice.js
+++ practice.js
@@ -194,6 +194,61 @@ console.table([myValue, myName, myCity])
 
 const myString = "Anjali"
 const myNumber = 100
+const isAdmitted = true
+const outsideTemp = null
+let designation;
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof isAdmitted);
+console.log(typeof outsideTemp);
+console.log(typeof designation);
+
+console.log(typeof(myString));
+
+let score = 33
+let score1 = "33"
+let score2 = "33abc"
+let score3 = null
+let score4 = undefined
+let score5 = -1/0
+let score6 = true
+let score7 = false
+let score8 = "Anjali"
+let score9 = ""
+
+console.log(`Datatype of score ${typeof score} and value is ${score}`);
+console.log(`Datatype of score ${typeof score1} and value is ${score1}`);
+console.log(`Datatype of score ${typeof score2} and value is ${score2}`);
+console.log(`Datatype of score ${typeof score3} and value is ${score3}`);
+console.log(`Datatype of score ${typeof score4} and value is ${score4}`);
+console.log(`Datatype of score ${typeof score5} and value is ${score5}`);
+console.log(`Datatype of score ${typeof score6} and value is ${score6}`);
+console.log(`Datatype of score ${typeof score7} and value is ${score7}`);
+console.log(`Datatype of score ${typeof score8} and value is ${score8}`);
+console.log(`Datatype of score ${typeof score9} and value is ${score9}`);
+
+let name = new String("Anjali")
+console.log(`value of string is ${name} and datatype is ${typeof name}`);
+
+let number = new Number(100)
+console.log(`value of number is ${number} and datatype is ${typeof number}`);
+
+let logged = new Boolean(true)
+console.log(`value of number is ${logged} and datatype is ${typeof logged}`);
+
+let outsideWeather = null 
+console.log(`value of number is ${outsideWeather} and datatype is ${typeof outsideWeather}`);
+
+let accountCity = undefined
+console.log(`value of number is ${accountCity} and datatype is ${typeof accountCity}`);
+
+let id = Symbol()
+console.log(`value of number is ${id} and datatype is ${typeof id}`);
+
+
+
+
 
 
 

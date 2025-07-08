e2d99a983307ce380cc7a689b20289d977073355
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-08 23:25:43 +0530

    upload

--- 07_projects/07_projects/chaiaurcode.js
+++ 07_projects/07_projects/chaiaurcode.js
@@ -25,6 +25,30 @@ buttons.forEach( function (button) {
          if(e.target.id == 'purple'){
             body.style.backgroundColor = e.target.id
          }
+
+         switch (e.target.id) {
+            case "grey":
+                body.style.backgroundColor = e.target.id
+               break;
+         
+            case "white":
+                body.style.backgroundColor = e.target.id
+               break;
+
+            case "blue":
+                body.style.backgroundColor = e.target.id
+               break;   
+
+            case "yellow":
+                body.style.backgroundColor = e.target.id
+               break; 
+               
+            case "purple":
+                body.style.backgroundColor = e.target.id
+               break;   
+            default:
+               break;
+         }
      })
 })
 

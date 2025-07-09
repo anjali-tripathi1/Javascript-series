ae90a8b8ca22c6b2d66760c00f577c5ea445eefe
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-09 13:23:40 +0530

    upload

--- GuessTheNumber/chaiaurcode.js
+++ GuessTheNumber/chaiaurcode.js
@@ -14,5 +14,63 @@ let numGuess = 1
 
 let playGame = true
 
+if (playGame) {
+   submit.addEventListener('click', function (e) {
+      e.preventDefault()
+      const guess = parseInt(userInput.value)
+      console.log(guess)
+      validateGuess(guess)
+   })
+}
 
+
+function validateGuess (guess) {
+    if(isNaN(guess)){
+        alert('please enter a valid number')
+    } else if (guess < 1) {
+        alert('please enter a number more than 1')
+    } else if (guess > 100) {
+        alert('please enter a number less than 100')
+    } else {
+         prevGuess.push(guess)
+         if (numGuess === 11) {
+              displayGuess(guess)
+              displayMessage(`GameOver, Random Number was ${randomNumber} `)
+              gameEnd()
+         } else {
+            displayGuess(guess)
+            checkGuess(guess)
+         }
+    }
+}
+
+function checkGuess (guess) {
+    if (guess === randomNumber) {
+          displayMessage("you guessed it right")
+          gameEnd()
+    }  else if (guess < randomNumber){
+          displayMessage(`Number is Tooo low`)
+    }  else if (guess > randomNumber) {
+        displayMessage(`Number is Tooo high`)
+    }
+}
+
+function displayGuess (guess){
+   userInput.value = ''
+   guessSlot.innerHTML += `${guess},  ` 
+   numGuess++
+   remaining.innerHTML = `${11 - numGuess}`
+}
+
+function displayMessage (message) {
+  lowOrHi.innerHTML = `<h2>${message}</h2>`
+}
+
+function gameEnd () {
+
+}
+
+function gameStart () {
+
+}
 

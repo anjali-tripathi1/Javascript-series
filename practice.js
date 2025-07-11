7265c06d349910dadf81fabf1679daa711db5ffd
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-11 12:32:53 +0530

    upload

--- practice.js
+++ practice.js
@@ -1,3 +1,53 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+ console.log(myArray[1]);
+ 
+let array = []
+
+for (let index = 0; index < myArray.length; index++ ){
+    const element = myArray[index]
+       console.log(element);
+}
+
+function addTwoNumbers (num1, num2) {
+     console.log(num1 + num2);
+     
+}
+
+addTwoNumbers(4, 5)
+
+
+function myArr (myArray) {
+    for (let i = 0; i < myArray.length; i++){
+          const element = myArray[i]
+          console.log(element);
+          
+    }
+      
+}
+myArr(myArray)
+        
+
+       
+let a;
+let result = a ?? 5;
+let b = 10;
+let result2 = b ?? 15;
+console.log(result);
+console.log(result2);
+
+function number (num1) {
+    for (let i = 1; i <= 10; i++){
+          console.log( (num1 * i) + 5);
+          
+    }
+    
+    
+}
+
+number(2)
+
+
+
 
 
 

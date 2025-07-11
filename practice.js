921f73536b99cb82ac4e6e6ce3685956335a9589
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-11 13:59:02 +0530

    upload

--- practice.js
+++ practice.js
@@ -1,51 +1,60 @@
-const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
- console.log(myArray[1]);
+// const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//  console.log(myArray[1]);
  
-let array = []
+// let array = []
 
-for (let index = 0; index < myArray.length; index++ ){
-    const element = myArray[index]
-       console.log(element);
-}
+// for (let index = 0; index < myArray.length; index++ ){
+//     const element = myArray[index]
+//        console.log(element);
+// }
 
-function addTwoNumbers (num1, num2) {
-     console.log(num1 + num2);
+// function addTwoNumbers (num1, num2) {
+//      console.log(num1 + num2);
      
-}
+// }
 
-addTwoNumbers(4, 5)
+// addTwoNumbers(4, 5)
 
 
-function myArr (myArray) {
-    for (let i = 0; i < myArray.length; i++){
-          const element = myArray[i]
-          console.log(element);
+// function myArr (myArray) {
+//     for (let i = 0; i < myArray.length; i++){
+//           const element = myArray[i]
+//           console.log(element);
           
-    }
+//     }
       
-}
-myArr(myArray)
+// }
+// myArr(myArray)
         
 
        
-let a;
-let result = a ?? 5;
-let b = 10;
-let result2 = b ?? 15;
-console.log(result);
-console.log(result2);
+// let a;
+// let result = a ?? 5;
+// let b = 10;
+// let result2 = b ?? 15;
+// console.log(result);
+// console.log(result2);
 
-function number (num1) {
-    for (let i = 1; i <= 10; i++){
-          console.log( (num1 * i) + 5);
+// function number (num1) {
+//     for (let i = 1; i <= 10; i++){
+//           console.log( (num1 * i) + 5);
           
-    }
+//     }
     
     
+// }
+
+// number(2)
+
+
+const myValue = {
+    coursename : "Js Series",
+    price : 599,
+    courseInstructor : "Anjali"
 }
 
-number(2)
-
+const {coursename : c}  = myValue
+console.log(c);
 
 
 

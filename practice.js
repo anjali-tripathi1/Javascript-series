c13f6afb37d87f203398f7500168439a111a8871
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-11 16:07:36 +0530

    upload

--- practice.js
+++ practice.js
@@ -56,6 +56,208 @@ const myValue = {
 const {coursename : c}  = myValue
 console.log(c);
 
+const {price : pr} = myValue
+console.log(pr);
+
+
+function addTwoNumbers (number1, number2) {
+    let result = number1 + number2
+    return result
+}
+
+const result = addTwoNumbers(4, 6)
+console.log(result);
+
+function loginUserMessage (username) {
+    if (username === undefined) {
+           console.log(`please enter your name`)
+           return
+    }
+
+    // if (!username) {
+    //     console.log(`please enter your name`);
+    //     return
+        
+    // }
+      console.log(`${username}, just logged in`);
+      
+}
+
+// loginUserMessage("Anjali")
+// loginUserMessage("")
+loginUserMessage()
+
+function calculateCartPrice (...num1) {
+      console.log(num1);
+      return num1
+    
+    }
+
+    console.log(calculateCartPrice(200, 300, 500, 700, 800));
+
+    const user = {
+        coursename : "Js Series",
+        price : 599,
+
+    }
+
+    function handlefunction (object) {
+          console.log(`username is ${object.coursename} and price is ${object.price}`);
+          
+    }
+
+    handlefunction(user)
+
+    handlefunction({
+        coursename : "Java",
+        price : 999
+    })
+
+    
+    function handleobject (anyobject) {
+          console.log(`username is : ${anyobject.name} and age is : ${anyobject.age}`);
+          
+    }
+
+    handleobject({
+        name : "Anjali",
+        age : 20
+    })
+
+    const course = {
+        coursename : "python",
+        price : 12999
+    }
+
+    function handleCourse(co) {
+         console.log(`coursename is : ${co.coursename} and price is : ${co.price}`);
+         
+    }
+
+    handleCourse(course)
+
+    handleCourse({
+        coursename : "Mobile Development",
+        price : 699
+    })
+
+    const getArray = [100, 200, 300, 400, 500]
+    function handleArray (arr) {
+          return arr
+         
+    }
+
+    console.log(handleArray(getArray))
+    console.log(handleArray([200, 300, 500, 800, 1000]));
+    console.log(handleArray(["Anjali", "shaktiman", "goga"]));
+
+    var g = 300
+    if (true) {
+        let a = 10
+        const b = 20
+        console.log(a);
+        console.log(b);
+        
+    }
+
+    // console.log(a);
+    // console.log(b);
+    console.log(g);
+    
+    function one(){
+        const username = "Anjali"
+
+        function two(){
+            const website = "youtube"
+            console.log(username);
+            
+        }
+
+        // console.log(website);
+        
+        two()
+    }
+
+    one()
+
+    if (true) {
+        const username = "Anjali"
+        if (username === "Anjali") {
+            const website = " youtube"
+            console.log(username + website);
+            
+        }  
+        
+        // console.log(website);
+        
+    }
+
+    // console.log(username);
+
+    console.log(addone(9))
+
+    function addone (num1) {
+        return  num1 + 3
+    }
+
+    
+
+    const addTwo = function (num1) {
+         return num1 + 6
+          
+    }
+
+   console.log(addTwo(7));
+
+   const myUser = {
+      username : "Anjali",
+      age : 20,
+
+      welcomeMessage : function () {
+          console.log(`username is : ${this.username} and age is : ${this.age}`)
+          console.log(this);
+          
+      }
+   }
+
+//    myUser.welcomeMessage()
+//    myUser.username = "Saroj"
+//    myUser.age = 35
+//    myUser.welcomeMessage()
+
+   console.log(this);
+   
+   function chai () {
+       console.log(this)
+   }
+    
+   chai()
+
+//    function chai2 () {
+//        let username = "Anjali"
+//        console.log(this.username);
+
+       
+//    }
+
+//    chai2()
+
+   const chai3 = function () {
+       console.log(this);
+    //    let username = "Anjali"
+    //    console.log(this.username);
+       
+   }
+
+   chai3()
+
+  
+    
+
+
+    
+    
+
 
 
 

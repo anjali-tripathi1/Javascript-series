1bda60611e2dcb3ed80c11baeef243e6b37d09ce
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-23 21:16:05 +0530

    upload

---
+++ unlimitedColors/chaiaurcode.js
@@ -0,0 +1,31 @@
+ 
+ const randomColor = function () {
+      const hex = '0123456789ABCDEF'
+      let color = '#' 
+      for(let i = 0; i < 6; i++){
+           color += hex[Math.floor(Math.random() * 16)]
+      }
+      return color
+ }
+
+ let interValid;
+
+ const startChangingColor = function () {
+    if(!interValid){
+         interValid = setInterval(changeBgColor, 1000)
+    }
+      
+
+    function changeBgColor () {
+       document.body.style.backgroundColor = randomColor()
+    } 
+ }
+ const stopChangingColor = function () {
+     clearInterval(interValid)
+     interValid = null
+ }
+ 
+
+ document.querySelector('#start').addEventListener('click', startChangingColor)
+
+ document.querySelector('#stop').addEventListener('click', stopChangingColor)

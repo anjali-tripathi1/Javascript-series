ecdab1177b6dada1fa093df5891b5c3932bab2f4
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-24 09:16:23 +0530

    upload

--- promises.js
+++ promises.js
@@ -12,3 +12,14 @@
     promiseOne.then(function () {
          console.log("promise consumed")
     })
+
+
+    new Promise( function (resolve, reject) {
+         setTimeout(function () {
+             console.log("Async 2 is called")
+             resolve()
+         }, 1000)
+    }).then(function () {
+       console.log("Async 2 resolved");
+       
+    })

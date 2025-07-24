af20471528718a7caf2694b18b6507884358b799
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-24 09:55:35 +0530

    upload

--- promises.js
+++ promises.js
@@ -23,3 +23,13 @@
        console.log("Async 2 resolved");
        
     })
+
+    const promiseThree = new Promise(function (resolve, reject) {
+         setTimeout(function () {
+          resolve({username : "chai", email : "chai@example.com"})
+         }, 1000)
+    })
+
+    promiseThree.then(function (user) {
+        console.log(user)
+    })

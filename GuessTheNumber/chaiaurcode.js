4531380fb5347e7221ae4cc4055706ebf4c2d778
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-28 23:32:44 +0530

    upload

--- GuessTheNumber/chaiaurcode.js
+++ GuessTheNumber/chaiaurcode.js
@@ -95,3 +95,53 @@ let randomNumber = Math.random() * 100 + 1
 const submit = document.querySelector('#subt')
 const userInput = document.querySelector('#guessField')
 const guessSlot = document.querySelector('.guesses')
+const lastResult = document.querySelector('.lastResult')
+const lowOrHi = document.querySelector('.lowOrHi')
+const startOver = document.querySelector('.resultParas')
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
+
+if(playGame){
+   submit.addEventListener('click', (e) => {
+       e.preventDefault()
+       const guess = parseInt(userInput.value)
+       console.log(guess);
+   })
+   
+}
+
+function validateGuess (guess) {
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    } else if(guess < 1){
+        alert('Number should be greater than 0')
+    } else if (guess > 100) {
+        alert('Number should be less than 100')
+    } else {
+         prevGuess.push(guess)
+         if(numGuess === 11){
+              displayGuess(guess)
+              displayMessage(`Game Over. Random number was ${randomNumber}`)
+              endGame()
+            } else {
+                 displayGuess(guess)
+                 checkGuess(guess)
+            }
+    }
+
+    function checkGuess (guess) {
+         if(guess === randomNumber){
+              displayMessage("You guessed it right")
+              endGame()
+         } else if (guess < randomNumber) {
+              displayMessage("Number is too low")
+         }  else if (guess > randomNumber) {
+              displayMessage("Number is too high")
+         }
+    }
+}

b2b9f3bd737b51632063f12b7a9f9f2edb66d829
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-28 18:43:05 +0530

    upload

--- BMICalculator/chaiaurcode.js
+++ BMICalculator/chaiaurcode.js
@@ -1,62 +1,34 @@
-// const form =  document.querySelector('form')
-// // this usecase will give you empty value
-// // const height = parseInt(document.querySelector('#height').value)
+const form =  document.querySelector('form')
+// this usecase will give you empty value
+// const height = parseInt(document.querySelector('#height').value)
 
-// form.addEventListener('submit', function (e) {
-//       e.preventDefault()
+form.addEventListener('submit', function (e) {
+      e.preventDefault()
 
-//       const height = parseInt(document.querySelector('#height').value)
-//       const weight = parseInt(document.querySelector('#weight').value)
-//       const results = document.querySelector('#results')
+      const height = parseInt(document.querySelector('#height').value)
+      const weight = parseInt(document.querySelector('#weight').value)
+      const results = document.querySelector('#results')
 
-//       if(height === '' || height < 0 || isNaN(height) ){
-//              results.innerHTML = `please give a valid height ${height}`
-//       } else if(weight === '' || weight < 0 || isNaN(weight) ){
-//              results.innerHTML = `please give a valid weight ${weight}`
-//       } else {
-//           const bmi = (weight / ((height * height) / 10000 )).toFixed(2)
-//           // show result 
-//           results.innerHTML  = `<span>${bmi}</span>`
-//          if(bmi < 18.6){
-//               results.innerHTML = `Under weight ${bmi}`
-//           } else if(bmi == 18.6 || bmi < 24.9){
-//               results.innerHTML = `Normal Range ${bmi}`
-//           } else if(bmi > 24.9 ){
-//               results.innerHTML = `Overweight ${bmi}`
-//           } 
+      if(height === '' || height < 0 || isNaN(height) ){
+             results.innerHTML = `please give a valid height ${height}`
+      } else if(weight === '' || weight < 0 || isNaN(weight) ){
+             results.innerHTML = `please give a valid weight ${weight}`
+      } else {
+          const bmi = (weight / ((height * height) / 10000 )).toFixed(2)
+          // show result 
+          results.innerHTML  = `<span>${bmi}</span>`
+         if(bmi < 18.6){
+              results.innerHTML = `Under weight ${bmi}`
+          } else if(bmi == 18.6 || bmi < 24.9){
+              results.innerHTML = `Normal Range ${bmi}`
+          } else if(bmi > 24.9 ){
+              results.innerHTML = `Overweight ${bmi}`
+          } 
       
-//       }
+      }
        
-// })
-
-
-const form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    const height = parseInt(document.querySelector('#height').value)
-    const weight = parseInt(document.querySelector('#weight').value)
-    const results = document.querySelector('#results')
-
-    if(height == '' || height < 0 || isNaN(height)){
-         results.innerHTML = `Please give a valid height ${height}`
-    }
-
-    else if(weight == '' || weight < 0 || isNaN(weight)){
-         results.innerHTML = `Please give a valid weight ${weight}`
-    } else {
-           const bmi = (weight / ((height * height) /10000)).toFixed(2)
-           results.innerHTML = `<span>${bmi}</span>`
-           if(bmi < 18.6){
-               results.innerHTML = `under weight ${bmi}`
-           } else if (bmi == 18.6 || bmi < 24.6) {
-               results.innerHTML = `Normal Range ${bmi}`
-           }  else if(bmi > 24.6){
-                results.innerHTML = `Overweight ${bmi}`
-           }
-    }
-
-   
 })
+
+
 
 
--- DigitalClock/chaiaurcode.js
+++ DigitalClock/chaiaurcode.js
@@ -6,5 +6,6 @@
 setInterval(function () {
 let date = new Date()
 // console.log(date.toLocaleTimeString());
-clock.innerHTML = date.toLocaleTimeString()
+// clock.innerHTML = date.toLocaleTimeString()
 }, 1000)
+

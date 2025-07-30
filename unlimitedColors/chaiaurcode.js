343cb927f3acbe56dadec1fb73877f08d66eacde
Anjali Tripatathi <anjalitripathi.kit@gmail.com>
2025-07-30 08:58:30 +0530

    upload

--- unlimitedColors/chaiaurcode.js
+++ unlimitedColors/chaiaurcode.js
@@ -1,31 +1,44 @@
  
- const randomColor = function () {
-      const hex = '0123456789ABCDEF'
-      let color = '#' 
-      for(let i = 0; i < 6; i++){
-           color += hex[Math.floor(Math.random() * 16)]
-      }
-      return color
- }
+//  const randomColor = function () {
+//       const hex = '0123456789ABCDEF'
+//       let color = '#' 
+//       for(let i = 0; i < 6; i++){
+//            color += hex[Math.floor(Math.random() * 16)]
+//       }
+//       return color
+//  }
 
- let interValid;
+//  let interValid;
 
- const startChangingColor = function () {
-    if(!interValid){
-         interValid = setInterval(changeBgColor, 1000)
-    }
+//  const startChangingColor = function () {
+//     if(!interValid){
+//          interValid = setInterval(changeBgColor, 1000)
+//     }
       
 
-    function changeBgColor () {
-       document.body.style.backgroundColor = randomColor()
-    } 
- }
- const stopChangingColor = function () {
-     clearInterval(interValid)
-     interValid = null
- }
+//     function changeBgColor () {
+//        document.body.style.backgroundColor = randomColor()
+//     } 
+//  }
+//  const stopChangingColor = function () {
+//      clearInterval(interValid)
+//      interValid = null
+//  }
  
 
- document.querySelector('#start').addEventListener('click', startChangingColor)
+//  document.querySelector('#start').addEventListener('click', startChangingColor)
 
- document.querySelector('#stop').addEventListener('click', stopChangingColor)
+//  document.querySelector('#stop').addEventListener('click', stopChangingColor)
+
+
+const randomColor = function () {
+     const hex = '0123456789ABCDEF'
+     let color = '#'
+     for(let i = 0; i < 6; i++){
+         color += hex[Math.floor(Math.random() * 16)]
+     }
+     return color
+}
+
+console.log(randomColor());
+ 
